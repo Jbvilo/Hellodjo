3d9b30ee55af79bf8576f0d33e6c071881fb8fe8
DESKTOP-5TA0IDD\jbvil <jbvilo@hotmail.com>
2022-06-01 01:07:01 +0200

    second commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  mobile: boolean = false;
+
 
   constructor() { }
 
   ngOnInit(): void {
+    if (window.innerWidth <= 960) {
+      this.mobile = true;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    console.log(event.target.innerWidth)
+    if(event.target.innerWidth <= 960) {
+      this.mobile = true
+    }
+    else {
+      this.mobile = false
+    }
   }
 
 }
